a595e68916524d4d1b0c4f2d50bde6c0c8dc4baa
Ilya Zlobintsev <ilya.zl@protonmail.com>
2023-07-14 22:23:18 +0300

    properly parse quoted and escaped arguments in requests

--- Mpdfin.Tests/Parsing.cs
+++ Mpdfin.Tests/Parsing.cs
@@ -5,16 +5,32 @@ public class Parsing
     [Fact]
     public void Basic()
     {
-        Request request = new("pause 1");
+        Request request = new("playlistinfo 0 10");
+        Assert.Equal(Command.playlistinfo, request.Command);
+        Assert.Equal(new List<string>() { "0", "10" }, request.Args);
+    }
+
+    [Fact]
+    public void QuotedArgument()
+    {
+        Request request = new("pause \"1\"");
         Assert.Equal(Command.pause, request.Command);
         Assert.Equal(new List<string>() { "1" }, request.Args);
     }
 
-    // [Fact]
-    // public void ArgumentInQuotes()
-    // {
-    //     Request request = new("pause \"1\"");
-    //     Assert.Equal(Command.pause, request.Command);
-    //     Assert.Equal(new List<string>() { "1" }, request.Args);
-    // }
+    [Fact]
+    public void QuotedWithSpace()
+    {
+        Request request = new("find \"multi word arg\"");
+        Assert.Equal(Command.find, request.Command);
+        Assert.Equal(new List<string>() { "multi word arg" }, request.Args);
+    }
+
+    [Fact]
+    public void EscapedQuotesInArgument()
+    {
+        Request request = new("find \"multi \\\"word\\\" arg\"");
+        Assert.Equal(Command.find, request.Command);
+        Assert.Equal(new List<string>() { "multi \"word\" arg" }, request.Args);
+    }
 }
--- Mpdfin/Mpd/Request.cs
+++ Mpdfin/Mpd/Request.cs
@@ -1,4 +1,6 @@
 using System.Globalization;
+using System.Text;
+using Serilog;
 
 namespace Mpdfin;
 
@@ -15,6 +17,7 @@ public enum Command
     addid,
     playlistinfo,
     plchanges,
+    find,
 
     command_list_begin,
     command_list_ok_begin,
@@ -28,31 +31,109 @@ public readonly record struct Request
 
     public Request(string raw)
     {
-        var split = raw.Split();
+        var chars = raw.ToCharArray();
 
-        if (split.Length != 0)
+        StringBuilder rawCommandBuilder = new();
+        int i;
+
+        for (i = 0; i < chars.Length; i++)
         {
-            var rawCommand = split[0];
-
-            // Make sure the command isn't a numeric representation of the enum
-            if (!int.TryParse(rawCommand, out int _) && Enum.TryParse(rawCommand, false, out Command command))
+            var c = chars[i];
+            if (c == ' ')
             {
-                Command = command;
-                Args = new();
+                var rawCommand = rawCommandBuilder.ToString();
 
-                for (int i = 1; i < split.Length; i++)
+                if (!int.TryParse(rawCommand, out int _) && Enum.TryParse(rawCommand, false, out Command command))
                 {
-                    Args.Add(split[i]);
+                    Command = command;
+                    break;
+                }
+                else
+                {
+                    throw new Exception($"unknown command {rawCommand}");
                 }
             }
             else
             {
-                throw new Exception($"unknown command {rawCommand}");
+                rawCommandBuilder.Append(c);
             }
         }
-        else
+
+        Args = new();
+        StringBuilder currentArgBuilder = new();
+
+        for (; i < chars.Length; i++)
         {
-            throw new Exception("Empty line provided");
+            var c = chars[i];
+            switch (c)
+            {
+                case '"':
+                    if (currentArgBuilder.Length > 0)
+                    {
+                        throw new Exception($"Unexpected data before quote: {currentArgBuilder}");
+                    }
+
+                    for (i++; i < chars.Length; i++)
+                    {
+                        var innerC = chars[i];
+
+                        switch (innerC)
+                        {
+                            case '"':
+                                var arg = currentArgBuilder.ToString();
+                                Args.Add(arg);
+                                currentArgBuilder.Clear();
+                                break;
+                            case '\\':
+                                i++;
+
+                                if (i < chars.Length)
+                                {
+                                    var escapedChar = chars[i];
+                                    currentArgBuilder.Append(escapedChar);
+                                }
+                                else
+                                {
+                                    throw new Exception("No character after escape symbol");
+                                }
+                                break;
+                            default:
+                                currentArgBuilder.Append(innerC);
+                                break;
+                        }
+                    }
+                    break;
+                case ' ':
+                    if (currentArgBuilder.Length > 0)
+                    {
+                        var arg = currentArgBuilder.ToString();
+                        Args.Add(arg);
+                        currentArgBuilder.Clear();
+                    }
+                    break;
+                case '\\':
+                    i++;
+
+                    if (i < chars.Length)
+                    {
+                        var escapedChar = chars[i];
+                        currentArgBuilder.Append(escapedChar);
+                    }
+                    else
+                    {
+                        throw new Exception("No character after escape symbol");
+                    }
+                    break;
+                default:
+                    currentArgBuilder.Append(c);
+                    break;
+            }
+        }
+
+        if (currentArgBuilder.Length > 0)
+        {
+            var arg = currentArgBuilder.ToString();
+            Args.Add(arg);
         }
     }
 }
